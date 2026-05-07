8b3b1b39c4c9d688ae769a92bc426f689c961edd
Mahmoud Awad <mahmoudawad112001@gmail.com>
2026-05-07 13:53:46 +0200

    feat: add division logic module

---
+++ scripts/div.js
@@ -0,0 +1,10 @@
+const firstnum = require('./firstnum.js');
+const secondnum = require('./secondnum.js');
+
+
+
+function div(firstnum, secondnum) {
+   return(firstnum / secondnum); 
+}
+
+module.exports = div;
