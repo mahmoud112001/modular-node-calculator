5adc2fedd2a122e6e755440e4505b020f84b4399
Mahmoud Awad <mahmoudawad112001@gmail.com>
2026-05-07 13:53:45 +0200

    feat: add multiplication logic module

---
+++ scripts/mul.js
@@ -0,0 +1,8 @@
+const firstnum = require('./firstnum.js');
+const secondnum = require('./secondnum.js');
+
+function mul(firstnum, secondnum) {
+    return firstnum * secondnum;
+}
+
+module.exports = mul;
