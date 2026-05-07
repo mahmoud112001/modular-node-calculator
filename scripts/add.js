c25f202486b903f6eea6ed7b1b69f9ec12008f47
Mahmoud Awad <mahmoudawad112001@gmail.com>
2026-05-07 13:53:44 +0200

    feat: add addition logic module

---
+++ scripts/add.js
@@ -0,0 +1,8 @@
+const firstnum = require('./firstnum.js');
+const secondnum = require('./secondnum.js');
+
+function add (firstnum, secondnum) {
+    return firstnum + secondnum;
+}
+
+module.exports = add;
