454c0ccdeb64d19d02ede5142d6e0515f1b9ebee
Mahmoud Awad <mahmoudawad112001@gmail.com>
2026-05-07 13:53:45 +0200

    feat: add subtraction logic module

---
+++ scripts/sub.js
@@ -0,0 +1,8 @@
+const firstnum = require('./firstnum.js');
+const secondnum = require('./secondnum.js');
+
+function sub(firstnum, secondnum) {
+    return firstnum - secondnum;
+}
+
+module.exports = sub;   
